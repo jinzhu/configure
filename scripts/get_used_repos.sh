49c36f6bc8a252371cd41beb150f5f2b7387a0b0
Jinzhu <wosmvp@gmail.com>
2013-05-05 16:04:43 +0800

    Add scripts to get used repos

---
+++ scripts/get_used_repos.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+# Script to get all the PPA installed on a system
+for APT in `find /etc/apt/ -name \*.list`; do
+    grep -Po "(?<=^deb\s).*?(?=#|$)" $APT | while read ENTRY ; do
+        HOST=`echo $ENTRY | cut -d/ -f3`
+        USER=`echo $ENTRY | cut -d/ -f4`
+        PPA=`echo $ENTRY | cut -d/ -f5`
+        #echo sudo apt-add-repository ppa:$USER/$PPA
+        if [ "ppa.launchpad.net" = "$HOST" ]; then
+            echo sudo apt-add-repository ppa:$USER/$PPA
+        else
+            echo sudo apt-add-repository \'${ENTRY}\'
+        fi
+    done
+done
+
+echo
+echo
+
+for APT in `find /etc/apt/ -name \*.list`; do
+    grep -o "^deb http://ppa.launchpad.net/[a-z0-9\-]\+/[a-z0-9\-]\+" $APT | while read ENTRY ; do
+        USER=`echo $ENTRY | cut -d/ -f4`
+        PPA=`echo $ENTRY | cut -d/ -f5`
+        echo sudo apt-add-repository ppa:$USER/$PPA
+    done
+done
