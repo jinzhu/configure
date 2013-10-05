dafd63aa097c2d036c521acff98b423126aff183
Jinzhu <wosmvp@gmail.com>
2013-10-05 23:00:41 +0800

    update emacs email configuration

--- files/emacs/personal/email.el
+++ files/emacs/personal/email.el
@@ -3,6 +3,21 @@
 (autoload 'wl-draft "wl-draft" "Write draft with Wanderlust." t)
 (setq wl-temporary-file-directory "~/.cache/temp")
 
+(setq wl-summary-always-sticky-folder-list t)
+(setq wl-summary-line-format "%n%T%P %D/%M (%W) %h:%m %t%[%25(%c %f%) %] %s")
+(setq wl-summary-width 150)
+
+;; HTML Email
+(require 'w3m)
+(require 'mime-w3m)
+
+(setq mime-view-type-subtype-score-alist
+      '(((text . plain) . 4)
+        ((text . enriched) . 3)
+        ((text . html) . 2)
+        ((text . richtext) . 1)))
+(setq wl-summary-incorporate-marks '("N" "U" "!" "A" "F" "$"))
+
 ;; IMAP, gmail:
 (setq elmo-imap4-default-server "imap.gmail.com"
       elmo-imap4-default-user "wosmvp@gmail.com"
@@ -31,9 +46,57 @@
       wl-trash-folder   "%[Gmail]/Trash"
       wl-fcc            "%[Gmail]/Sent"
 
+      wl-folder-check-async t
+
       ;; mark sent messages as read (sent messages get sent back to you and
       ;; placed in the folder specified by wl-fcc)
       wl-fcc-force-as-read    t
-
       ;;for when auto-compleating foldernames
-      wl-default-spec "%")
+      wl-default-spec "%"
+
+      wl-auto-save-drafts-interval nil
+      wl-stay-folder-window t                       ;; show the folder pane (left)
+      wl-folder-window-width 25                     ;; toggle on/off with 'i'
+
+      wl-message-ignored-field-list '("^.*:")
+      wl-message-visible-field-list
+      '("^\\(To\\|Cc\\):"
+        "^Subject:"
+        "^\\(From\\|Reply-To\\):"
+        "^Organization:"
+        "^Message-Id:"
+        "^\\(Posted\\|Date\\):"
+        )
+      wl-message-sort-field-list
+      '("^From"
+        "^Organization:"
+        "^X-Attribution:"
+        "^Subject"
+        "^Date"
+        "^To"
+        "^Cc"))
+
+
+;; http://emacs-fu.blogspot.com/2009/08/managing-e-mail-addresses-with-bbdb.html
+(require 'bbdb-wl)
+(bbdb-wl-setup)
+(setq bbdb-wl-folder-regexp    ;; get addresses only from these folders
+ "^\.inbox$\\|^.sent")    ;;
+
+(require 'elmo-search)
+(elmo-search-register-engine
+ 'mu 'local-file
+ :prog "mu" ;; or wherever you've installed it
+ :args '("find" pattern "--fields" "l") :charset 'utf-8)
+
+(setq elmo-search-default-engine 'mu)
+;; for when you type "g" in folder or summary.
+(setq wl-default-spec "[")
+
+(setq wl-biff-check-folder-list '("%inbox"))
+(setq wl-biff-check-interval 10)
+(add-hook 'wl-biff-notify-hook
+          '(lambda()
+            (djcb-popup "Wanderlust" "You have new mail!"
+                        "/usr/share/icons/gnome/32x32/status/mail-unread.png"
+                        "/usr/share/sounds/freedesktop/stereo/message-new-instant.oga")))
--- files/emacs/personal/settings.el
+++ files/emacs/personal/settings.el
@@ -305,3 +305,23 @@
 
 ;; Smex
 (global-set-key (kbd "<menu>") 'smex)
+
+;; Gnutls
+(setq gnutls-min-prime-bits 2048)
+
+
+(defun djcb-popup (title msg &optional icon sound)
+  "Show a popup if we're on X, or echo it otherwise; TITLE is the title
+of the message, MSG is the context. Optionally, you can provide an ICON and
+a sound to be played"
+
+  (interactive)
+  (when sound (shell-command
+               (concat "mplayer -really-quiet " sound " 2> /dev/null")))
+  (if (eq window-system 'x)
+      (shell-command (concat "notify-send "
+
+                             (if icon (concat "-i " icon) "")
+                             " '" title "' '" msg "'"))
+    ;; text only version
+    (message (concat title ": " msg))))
