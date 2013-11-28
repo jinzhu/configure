f1f9ae6116bd2546d11628657dce06315f6c7c9e
Jinzhu <wosmvp@gmail.com>
2013-11-28 15:03:14 +0800

    emmet-mode default html indentation is 2

--- emacs/settings/programming.el
+++ emacs/settings/programming.el
@@ -38,6 +38,7 @@
 
 (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
 (add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
+(add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2)))
 
 (setq web-beautify-args  '("--indent-size" "2" "-f" "-"))
 
